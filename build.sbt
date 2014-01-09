ed368342088fa9c31cb8a7ce6033bbbbbb7a6fe1
Jakob Jakobsen Boysen <jakob@jboysen.dk>
2014-01-09 09:53:59 +0100

    added logging, setup fetcher flow

--- build.sbt
+++ build.sbt
@@ -15,6 +15,7 @@ libraryDependencies ++= Seq(
   "io.spray" % "spray-http" % "1.2.0",
   "com.typesafe.akka" %% "akka-actor" % "2.2.3",
   "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
-  "org.json4s" %% "json4s-native" % "3.2.6"
+  "org.json4s" %% "json4s-native" % "3.2.6",
+  "com.typesafe" %% "scalalogging-slf4j" % "1.0.1"
 )
 
--- src/main/scala/iprice/boot/Fetcher.scala
+++ src/main/scala/iprice/boot/Fetcher.scala
@@ -1,9 +1,25 @@
 package iprice.boot
 
-import fetcher.DBAFetcher
+import fetcher.{DBAParser, DBAFetcher}
+import com.typesafe.scalalogging.slf4j.Logging
 
-object Fetcher extends App {
+object Fetcher extends App with Logging {
+
+  logger.info("Starting fetcher...")
   val fetcher = new DBAFetcher()
+  val parser = new DBAParser(fetcher request)
+  val totalPages = parser getTotalPages
 
+  logger.debug("Initial values set. Start collecting ads.")
+
+  val allAds = for (page <- 1 to totalPages)
+  yield {
+    logger.debug("Getting page " + page + " of " + totalPages + "...")
+    fetcher setPage (page)
+    parser setRawInput (fetcher request)
+    parser getAds
+  }
+
+  logger.debug("Done fetching pages...")
 }
 
